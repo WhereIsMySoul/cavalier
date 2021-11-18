ba1591ec8efa44cd98510eefa4860a6cd533e303
WhereIsMySoul <94597922+WhereIsMySoul@users.noreply.github.com>
2021-11-18 08:49:07 +0100

    Update cavalier.java

--- cavalier.java
+++ cavalier.java
@@ -1,1 +1,31 @@
-
+public class MiniProjet{
+	static class chevalier{
+		int position;
+		
+		boolean tomberTrou;
+		boolean tomberRiviere;
+		boolean excalibur;
+	}
+	
+	static int de(){
+		int de;
+		de = (int) (Math.random()*6+1);
+		return de;
+	}
+	
+	static void excalibur(chevalier knight){
+    int tirage
+		if (Knight.tomberriviere){
+      tirage = (int) (Math.random()*256)
+        
+    
+    }
+	}
+	
+	public static void main(String [] args){
+		int i;
+		for (i = 0;i<100;i = i + 1){
+			Ecran.afficher(de(),"\n");
+		}
+	}
+}
